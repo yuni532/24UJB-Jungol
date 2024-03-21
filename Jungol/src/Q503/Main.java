4e2ecf32ce1bdc81bf8c0d0ec4aa5591465bbd35
21-manic-005g <21-manic-005g@21-manic-005>
2024-03-21 10:31:18 +0900

    정올 503번 문제 통과

---
+++ Jungol/src/Q503/Main.java
@@ -0,0 +1,8 @@
+package Q503;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("My name is Hong Gil Dong.");
+		System.out.println("I am 13 years old.");
+	}
+}
